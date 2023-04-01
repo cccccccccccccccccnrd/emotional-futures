2b284eccba46cb00ebead964560818a0106f8d71
cccccccccccccccccnrd <hello@conradweise.com>
2023-04-01 21:08:15 +0200

    𝓂𝒶𝓍𝐻

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,8 @@
   <div class="h-screen flex flex-col justify-center items-center">
     <div
       class="h-full w-full relative overflow-hidden"
-      :class="isMobile ? '' : 'max-h-[676px] max-w-[311px] rounded-2xl border-2 border-white-20'"
-      :style="isMobile ? '' : 'filter: drop-shadow(0 1rem 1rem rgba(0, 0, 0, 0.3))'"
+      :class="isMobile ? '' : 'max-w-[311px] rounded-2xl border-2 border-white-20'"
+      :style="`max-height: ${maxH}px; ${isMobile ? '' : 'filter: drop-shadow(0 1rem 1rem rgba(0, 0, 0, 0.3));'}`"
     >
       <slot />
     </div>
@@ -17,4 +17,7 @@
 const isMobile = navigator.userAgent.match(
   /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
 )
+
+const max = 676
+const maxH = computed(() => window.innerHeight <= max ? window.innerHeight : max)
 </script>
