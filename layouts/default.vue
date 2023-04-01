f0f89dcd1c26d867913360096060c0ff72559de9
cccccccccccccccccnrd <hello@conradweise.com>
2023-04-01 21:29:59 +0200

    const h = window.innerHeight

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-center">
+  <div class="h-full flex flex-col justify-center items-center" :class="isMobile ? '' : 'h-screen'">
     <div
       class="h-full w-full relative overflow-hidden"
-      :class="isMobile ? '' : 'max-w-[311px] rounded-2xl border-2 border-white-20'"
-      :style="`max-height: ${maxH}px; ${isMobile ? '' : 'filter: drop-shadow(0 1rem 1rem rgba(0, 0, 0, 0.3));'}`"
+      :class="isMobile ? '' : 'max-h-[676px] max-w-[311px] rounded-2xl border-2 border-white-20'"
+      :style="`${isMobile ? `height: ${h}px;` : 'filter: drop-shadow(0 1rem 1rem rgba(0, 0, 0, 0.3));'}`"
     >
       <slot />
     </div>
@@ -18,6 +18,5 @@ const isMobile = navigator.userAgent.match(
   /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
 )
 
-const max = 676
-const maxH = computed(() => window.innerHeight <= max ? window.innerHeight : max)
+const h = window.innerHeight
 </script>
