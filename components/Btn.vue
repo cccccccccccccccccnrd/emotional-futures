a6d143070a4888854a6d6ed3fb48651b4e3f16fd
cccccccccccccccccnrd <hello@conradweise.com>
2023-04-12 18:55:06 +0200

    p-?

--- components/Btn.vue
+++ components/Btn.vue
@@ -2,14 +2,14 @@
   <div
     v-if="type === 'dark'"
     class="w-full flex justify-center items-center font-bold text-white-100 bg-dark-50 backdrop-blur-md border-2 border-transparent"
-    :class="[disabled ? '!bg-dark-20' : '', padding ? `p-${padding}` : 'p-2']"
+    :class="[disabled ? '!bg-white-20' : '', padding ? padding : 'p-2']"
   >
     <slot />
   </div>
   <div
     v-else
     class="w-full flex justify-center items-center font-bold text-dark-100 bg-white-100 border-2 border-transparent"
-    :class="[disabled ? '!bg-dark-20' : '', padding ? `p-${padding}` : 'p-2']"
+    :class="[disabled ? '!bg-white-20' : '', padding ? padding : 'p-2']"
   >
     <slot />
   </div>
@@ -20,5 +20,11 @@ const props = defineProps({
   type: String,
   disabled: Boolean,
   padding: String
+})
+
+const padding = computed(() => {
+  if (props.padding === '1') return 'p-1'
+  if (props.padding === '1.5') return 'p-1.5'
+  return 'p-2'
 })
 </script>
