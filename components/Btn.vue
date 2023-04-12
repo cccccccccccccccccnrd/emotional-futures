e051958cbeee0ed88653bcf7f63856979c269b56
cccccccccccccccccnrd <hello@conradweise.com>
2023-04-12 18:41:17 +0200

    !p-2

--- components/Btn.vue
+++ components/Btn.vue
@@ -2,18 +2,14 @@
   <div
     v-if="type === 'dark'"
     class="w-full flex justify-center items-center font-bold text-white-100 bg-dark-50 backdrop-blur-md border-2 border-transparent"
-    :class="`${disabled ? '!bg-white-20' : ''} ${
-      padding ? `p-${padding}` : 'p-2'
-    }`"
+    :class="[disabled ? '!bg-dark-20' : '', padding ? `p-${padding}` : 'p-2']"
   >
     <slot />
   </div>
   <div
     v-else
     class="w-full flex justify-center items-center font-bold text-dark-100 bg-white-100 border-2 border-transparent"
-    :class="`${disabled ? '!bg-white-20' : ''} ${
-      padding ? `p-${padding}` : 'p-2'
-    }`"
+    :class="[disabled ? '!bg-dark-20' : '', padding ? `p-${padding}` : 'p-2']"
   >
     <slot />
   </div>
